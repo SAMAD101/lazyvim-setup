b25d5591c78921194d0af26bca2e29bc658c96d3
Sam <asamadans@gmail.com>
2024-08-06 10:28:58 +0530

    terraform

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -57,6 +57,9 @@ return {
         },
         eslint = {},
         rust_analyzer = {},
+        terraformls = {
+          filetypes = { "terraform", "tf", "terraform-vars" },
+        },
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
