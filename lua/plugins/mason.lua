8112a81b16edf5d83ed53196a9fbcac22fcfe5e0
Sam <asamadans@gmail.com>
2024-08-06 06:32:58 +0530

    more configurations: lsps, linters....and more...

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,12 +1,27 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "stylua",
-      "shellcheck",
-      "shfmt",
-      "flake8",
-      "ruff",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "ruff",
+        "rust-analyzer",
+        "typescript-language-server",
+        "js-debug-adapter",
+      },
+    },
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "rust_analyzer",
+        "tsserver",
+        "eslint",
+      },
     },
   },
 }
